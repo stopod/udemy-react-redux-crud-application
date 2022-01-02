48db62098c2d486fa0b301a5621d3dce50615e18
stopod <stop_od@outlook.jp>
2022-01-02 16:49:10 +0900

    action

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+// action creator
+export const increment = () => {{
+    type: INCREMENT // action
+}}
+
+export const decrement = () => {{
+    type: DECREMENT
+}}
